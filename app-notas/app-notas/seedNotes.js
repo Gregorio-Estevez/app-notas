531eaa4f2006f9f347e50698cf09b3c50a330a3e
sebas <sebastian.mercado1415@gmail.com>
2023-09-14 12:57:41 -0400

    Cluster & seed fix

--- app-notas/app-notas/seedNotes.js
+++ app-notas/app-notas/seedNotes.js
@@ -1,1 +1,19 @@
-// Cargar los datos de prueba en la base de datos
+const Notes = require('./database');
+const notesJSON = require('./notes');
+const notes = JSON.parse(notesJSON);
+
+async function seedNotesToDatabase() {
+    const notesToSave = [];
+
+    for (const note of notes) {
+        const newNote = new Notes({
+            title: note.title,
+            description: note.description
+    });
+
+    notesToSave.push(newNote);
+  }
+  await Notes.bulkSave(notesToSave);
+}
+
+seedNotesToDatabase();
