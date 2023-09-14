527df86ea3a91f20704c32fd554dcf5a1e973c76
Jean Azar <140527289+Jcazt21@users.noreply.github.com>
2023-09-14 17:02:03 -0400

    Revert OpenTelemetry

--- app-notas/app-notas/server.js
+++ app-notas/app-notas/server.js
@@ -22,38 +22,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-
-// OpenTelemerty
-// // Importaciones
-// const express = require("express");
-// const { Collector, MeterProvider } = require("@opentelemetry/node");
-
-// // Inicialización de OpenTelemetry
-// const collector = new Collector();
-// const meterProvider = new MeterProvider(collector);
-
-// // Registro de datos de la aplicación
-// app.use((req, res, next) => {
-//   const span = meterProvider.createSpan("notes", {
-//     attributes: {
-//       client: req.headers["user-agent"],
-//       ip: req.connection.remoteAddress,
-//       queryParams: req.query,
-//       requestBody: req.body,
-//     },
-//   });
-
-//   next();
-
-//   span.end();
-// });
-
-// // Configuración del colector de OpenTelemetry (Puse el puerto default de SigNoz)
-// collector.setExporter({
-//   type: "otlp",
-//   endpoint: "http://localhost:9411/v1/traces",
-// });
-
-// // Inicio del servidor
-// app.listen(3000, () => console.log("Servidor iniciado en el puerto 3000"));
